04228415ae4f2d4b891e59c13ef807aef26895fe
Nicolas Alvarez Mondragon <nicogranuja@hotmail.com>
2016-08-23 15:49:04 -0500

    added dynamic text change for the description of the projects in the portfolio, and made the carousel movement false so it doesn't rotate by itself.

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
 	"use strict";
-	var $welcome = $('#welcomeMessage');
+	var $welcome = $('#welcomeMessage');//h1 welcome message
+	var $description = $('#projectDescrition');//p tag for describing the projects
 
+
+
+	function projectsCarousel(){
+		
+	}
 	//message that shows first greeting.
 	function welcomeMessage(){
 		$welcome.hide();
@@ -37,8 +43,45 @@ $(document).ready(function() {
 		//}
 	});
 	/*****End Smooth Scrolling*/
-
+	//function that checks for the click on the carousel right or left button to change the description of the projects.
+	$('.moveCarousel').click(function(){
+		var content = '<h3>Project Description</h3>';
+		switch($(".active").index()){
+			case 0:
+				content+='<p>Weather App website uses the weather app API as well as the Google Maps API.</p>';
+				content+= '<h3>Project Details:</h3>';
+				content+= '<ul>';
+				content+= '<li>It retrieves the weather conditions for the current and next two days.</li>';
+				content+= '<li>With the help of the google map you can easily change the location.</li>';
+				content+= '<li>Uses ajax requests to pull data from APIs.</li>';
+				content+= '</ul>';
+				break;
+			case 1:
+				content+='<p>Calculator App</p>';
+				content+= '<h3>Project Details:</h3>';
+				content+= '<ul>';
+				content+= '<li>The app takes two numbers or one number as input.</li>';
+				content+= '<li>Calculates the selected operation.</li>';
+				content+= '<li>Disables buttons so user input becomes easier.</li>';
+				content+= '</ul>';
+				break;
+			case 2:
+				content+='<p>It is the classic simple simon game.</p>';
+				content+= '<h3>Project Details:</h3>';
+				content+= '<ul>';
+				content+= '<li>The game is configured to start the sequence when the start game button is pressed.</li>';
+				content+= '<li>Sounds and animations will help the user have an immerse experience.</li>';
+				content+= '<li>It has animations for lighting up the buttons, starting the game, and ending the game.</li>';
+				content+= '</ul>';
+				break;
+			default:
+				console.log("default");
+		}
+		$description.html(content);
+	});	
 	welcomeMessage();
+	
+	
 	
 
 });
